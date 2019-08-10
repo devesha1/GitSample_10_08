258f3629ffb162bb9688d85aec3069f96b73c640
devesha1 <deveshideal2005@gmail.com>
2019-08-10 19:25:27 +0530

    First commit with .gitignore file

---
+++ GitSample_10_08/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSample_10_08
+//
+//  Created by Devesh Sharma on 10/08/19.
+//  Copyright © 2019 Devesh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
