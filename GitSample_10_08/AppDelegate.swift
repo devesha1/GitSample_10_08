e54ce0e0770865643ea1ed4eabd3293426abe145
devesha1 <deveshideal2005@gmail.com>
2019-11-07 23:36:03 +0530

    Update AppDelegate.swift
    Made changes from server

--- GitSample_10_08/AppDelegate.swift
+++ GitSample_10_08/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print("branch99 chnage1")
+        print("changes from server branch99")
         return true
     }
 
